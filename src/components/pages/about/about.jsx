2a6bef57f2705429d4d671ff94bbd42d1141849e
tatev55 <tatevmargaryan555@gmail.com>
2025-03-03 10:41:05 +0400

    fix

--- src/components/pages/about/about.jsx
+++ src/components/pages/about/about.jsx
@@ -1,11 +1,12 @@
 import "./about.css"
+import image from "../../../assets/images/bg-about.jpg"
 
 const About = ()=> {
     return (
         <div className="about-container">
             <h2 className="title-about">About Our Recipe Collection</h2>
             <div className="in-about-container">
-                <img src="src/assets/images/bg-about.jpg" alt=""  className="about-image"/>
+                <img src={image} alt=""  className="about-image"/>
                 <div className="about-text">
                     <p className="text">Welcome to our Recipe Collection, where we bring together a wide variety of delicious, easy-to-make, and healthy recipes to inspire your cooking adventures! Whether you're a seasoned chef or just starting to explore the world of home-cooking, you'll find something to satisfy your taste buds.
                         Our collection features a diverse range of recipes, from comforting classics to adventurous new dishes from all around the world. Each recipe is carefully selected and tested to ensure that you get the best quality and flavor in every bite. Whether you're looking for quick weeknight meals, impressive dinner party ideas, or indulgent desserts, we have something to suit every occasion.
--- src/components/pages/gallery/gallery.jsx
+++ src/components/pages/gallery/gallery.jsx
@@ -1,42 +1,49 @@
-import "./gallery.css"
+import "./gallery.css";
+import veganImage from "../../../assets/images/vegan.webp";
+import lasagneImage from "../../../assets/images/Lasagne.webp";
+import honeyImage from "../../../assets/images/Honey.webp";
+import garlicImage from "../../../assets/images/Garlic.jpg";
+import aaImage from "../../../assets/images/aa.jpg";
+import easyImage from "../../../assets/images/Easy.jpg";
+import pexelImage from "../../../assets/images/pexel.jpg";
+import eaImage from "../../../assets/images/Ea.jpg";
+import pancakesImage from "../../../assets/images/pancakes.webp";
 
-
-const Gallery = ()=> {
-    return (
-        <div className="container">
-            <h1 className="title">Gallery</h1>
-            <div className="gallery">
-                <div className="photo photo_tree">
-                    <img src="src/assets/images/vegan.webp" alt=""/>
-                </div>
-                <div className="photo">
-                    <img src="src/assets/images/Lasagne.webp" alt=""/>
-                </div>
-                <div className="photo">
-                    <img src="src/assets/images/Honey.webp" alt=""/>
-                </div>
-                <div className="photo for">
-                    <img src="src/assets/images/Garlic.jpg" alt=""/>
-                </div>
-                <div className="photo">
-                    <img src="src/assets/images/aa.jpg" alt=""/>
-                </div>
-                <div className="photo">
-                    <img src="src/assets/images/Easy.jpg" alt=""/>
-                </div>
-                <div className="photo photo_sunset">
-                    <img src="src/assets/images/pexel.jpg" alt=""/>
-                </div>
-                <div className="photo photo_flower_sunset">
-                    <img src="src/assets/images/Ea.jpg" alt=""/>
-                </div>
-                <div className="photo photo_flower">
-                    <img src="src/assets/images/pancakes.webp" alt=""/>
-                </div>
-           
-            </div>
+const Gallery = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Gallery</h1>
+      <div className="gallery">
+        <div className="photo photo_tree">
+          <img src={veganImage} alt="Vegan" />
         </div>
-    )
-}
+        <div className="photo">
+          <img src={lasagneImage} alt="Lasagne" />
+        </div>
+        <div className="photo">
+          <img src={honeyImage} alt="Honey" />
+        </div>
+        <div className="photo for">
+          <img src={garlicImage} alt="Garlic" />
+        </div>
+        <div className="photo">
+          <img src={aaImage} alt="AA" />
+        </div>
+        <div className="photo">
+          <img src={easyImage} alt="Easy" />
+        </div>
+        <div className="photo photo_sunset">
+          <img src={pexelImage} alt="Pexel" />
+        </div>
+        <div className="photo photo_flower_sunset">
+          <img src={eaImage} alt="EA" />
+        </div>
+        <div className="photo photo_flower">
+          <img src={pancakesImage} alt="Pancakes" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
